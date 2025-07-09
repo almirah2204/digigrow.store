a207c73fce5476a73aa6d80dfa81d1b2fbcd447a
almirah2204 <aliraza2668844@gmail.com>
2025-07-09 14:52:17 +0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to DigiGrow!</h1>
+      <p>This is your custom homepage! 🚀</p>
+    </main>
+  );
+}
